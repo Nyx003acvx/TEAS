6fbc725bdf119b20d87d5145fcad26798a3cf559
Shirswa Chakma <110004012+Nyx003acvx@users.noreply.github.com>
2025-09-11 15:00:26 +0600

    updating settings

--- tenvelop_employee_attendance_system/settings.py
+++ tenvelop_employee_attendance_system/settings.py
@@ -6,7 +6,7 @@ Replace your current settings.py with this version
 import os
 from pathlib import Path
 
-import dj_base_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
